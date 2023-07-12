fd5fe65f336633ebd0828480e19a5c92b9676263
Kyrill Poole <kyrill@geckoboard.com>
2023-07-12 16:17:04 +0100

    Update AWS connect for `fog-aws`
    The more recent version of `fog-aws` needed some changes made in order for it to work. While I was in here, I also ran the linter over the code to fix up some of the indentation and quotes.

--- lib/vagrant-aws/action/connect_aws.rb
+++ lib/vagrant-aws/action/connect_aws.rb
@@ -1,5 +1,5 @@
-require "fog"
-require "log4r"
+require 'fog/aws'
+require 'log4r'
 
 module VagrantPlugins
   module AWS
@@ -8,9 +8,9 @@ module VagrantPlugins
       # puts the AWS connection object into the `:aws_compute` key
       # in the environment.
       class ConnectAWS
-        def initialize(app, env)
+        def initialize(app, _env)
           @app    = app
-          @logger = Log4r::Logger.new("vagrant_aws::action::connect_aws")
+          @logger = Log4r::Logger.new('vagrant_aws::action::connect_aws')
         end
 
         def call(env)
@@ -22,23 +22,22 @@ module VagrantPlugins
 
           # Build the fog config
           fog_config = {
-            :provider => :aws,
-            :region   => region
+            provider: 'AWS',
+            region: region
           }
           if region_config.use_iam_profile
             fog_config[:use_iam_profile] = true
           else
-            fog_config[:aws_access_key_id] = region_config.access_key_id
+            fog_config[:aws_access_key_id]     = region_config.access_key_id
             fog_config[:aws_secret_access_key] = region_config.secret_access_key
-            fog_config[:aws_session_token] = region_config.session_token
+            fog_config[:aws_session_token]     = region_config.session_token
           end
 
-          fog_config[:endpoint] = region_config.endpoint if region_config.endpoint
-          fog_config[:version]  = region_config.version if region_config.version
+          fog_config[:version] = region_config.version if region_config.version
 
-          @logger.info("Connecting to AWS...")
+          @logger.info('Connecting to AWS...')
           env[:aws_compute] = Fog::Compute.new(fog_config)
-          env[:aws_elb]     = Fog::AWS::ELB.new(fog_config.except(:provider, :endpoint))
+          env[:aws_elb]     = Fog::AWS::ELB.new(fog_config.except(:provider))
 
           @app.call(env)
         end
